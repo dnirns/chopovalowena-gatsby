82380d98988e2c59260d8eefdceb513d70669ad6
Dan <danielirons@protonmail.com>
2021-12-25 10:28:50 +0000

    about page added / cms content added

---
+++ src/pages/about.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { graphql } from 'gatsby'
+import Layout from '../components/layout/Layout'
+
+const about = ({ data: { page } }) => {
+  console.log(page.edges[0])
+  return (
+    <Layout>
+      <p className='text-2xl mb-4'>{page.edges[0].node.aboutText.aboutText}</p>
+    </Layout>
+  )
+}
+
+export default about
+
+export const AboutQuery = graphql`
+  query AboutQuery {
+    page: allContentfulAboutPage {
+      edges {
+        node {
+          aboutText {
+            aboutText
+          }
+        }
+      }
+    }
+  }
+`
